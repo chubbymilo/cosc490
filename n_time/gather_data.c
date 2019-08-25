ae507fee2a57756370b36a14d0461cadca0fefd5
James <hazhao@DESKTOP-KHOL4L5.localdomain>
2019-08-25 14:38:45 +1200

    Update gather_data file with nanosleep function that sleeps between gathering data command.

--- n_time/gather_data.c
+++ n_time/gather_data.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "time.h"
+#include "stdlib.h"
+#include "unistd.h"
 /*
 void n_time(void){
     struct timespec ts;
@@ -46,7 +48,25 @@ void read_memory(const char* filename_in, const char* filename_out){
     fclose(fp);
 }
 int main(int argc, char *argv[]){
+    int i;
+    struct timespec sleep_time;
+    char *a = argv[3];
+    char *b = argv[4];
+    int repeat =atoi(a);
+    long sleep = atol(b);
+    sleep_time.tv_sec = 0;
+    sleep_time.tv_nsec = sleep;
+     /*
+    time_t start = time(NULL);
+    time_t now = time(NULL);
+    while ((now - start) <= 20) {
+        myfunc();
+        time_t now = time(NULL);
+    }*/
+    for(i = 0; i < repeat; i++){
+        read_memory(argv[1], argv[2]);
+        nanosleep(&sleep_time,NULL);
+    }
     //n_time();
     //read_file("/sys/fs/cgroup/memory/docker/972e3860906ba4392f9b28bfbe085eda6e58a68e8c995721bbd77c7ccc263978/memory.stat","/tmp/memory_test.txt");
-    read_memory(argv[1], argv[2]);
 }
