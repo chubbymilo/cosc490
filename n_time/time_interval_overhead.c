41ee144b108f5dbe78af6a834699098809ae6f6e
James <hazhao@DESKTOP-KHOL4L5.localdomain>
2019-08-29 22:06:30 +1200

    updated time_interval_overheads to display duration.

--- n_time/time_interval_overhead.c
+++ n_time/time_interval_overhead.c
@@ -21,12 +21,24 @@ char *b = argv[2];
 time_interval = atol(b);
 sleep_time.tv_sec = 0;
 sleep_time.tv_nsec = time_interval;
+char buffer[20];
+char buffer2[20];
+int len;
+long long start_time;
+long long end_time;
 
 timespec_get(&start, TIME_UTC);
 for (i = 0; i < repeat; i++){
     nanosleep(&sleep_time, NULL);
 }
 timespec_get(&end, TIME_UTC);
-printf ("Start time : %lld%09ld", (long long)start.tv_sec, start.tv_nsec);
-printf ("End time : %lld%09ld", (long long)end.tv_sec, end.tv_nsec);
+printf ("Start time : %lld%09ld\n", (long long)start.tv_sec, start.tv_nsec);
+printf ("End time : %lld%09ld\n", (long long)end.tv_sec, end.tv_nsec);
+len = sprintf(buffer,"%lld%09ld", (long long)start.tv_sec, start.tv_nsec);
+//printf("%d\n", len);
+len = sprintf(buffer2,"%lld%09ld", (long long)end.tv_sec, end.tv_nsec);
+start_time = atol(buffer);
+end_time = atol(buffer2);
+//printf("%lld\n", start_time);
+printf("Duration: %lld\n", end_time-start_time);
 }
